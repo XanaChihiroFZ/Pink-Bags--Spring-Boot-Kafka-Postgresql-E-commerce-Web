d43d9623f9dfd49490b92243075bf20276e4c667
Fereshteh Zahra <fereshtehzahra.official@gmail.com>
2026-08-14 14:52:43 +0330

    Delete pinkbags directory

--- pinkbags/pinkbags/src/main/java/com/ecommerce/pinkbags/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.pinkbags.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ecommerce.pinkbags.entities.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
